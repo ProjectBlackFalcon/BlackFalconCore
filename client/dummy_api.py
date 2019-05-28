3a58f706c49698a6e55e9cda454c5cceafb8b96d
avialaret <alexisvlrt@gmail.com>
2019-05-28 16:48:46 +0200

    Minor changes to the echo websocker server

--- client/dummy_api.py
+++ client/dummy_api.py
@@ -5,16 +5,20 @@ import time
 
 
 async def hello(websocket, path):
-    order = await websocket.recv()
-    order = ast.literal_eval(order.decode('utf8'))
-    greeting = "Recieved order : " + str(order)
-    while 1:
-        await websocket.send('A result')
-        time.sleep(1)
+    async for order in websocket:
+        order = ast.literal_eval(order)
+        greeting = "Recieved order : " + str(order)
+        await websocket.send(str(greeting))
+        print('API sent back response')
+
 
 def start_server(bot_id):
-    asyncio.set_event_loop(asyncio.new_event_loop())
+    # asyncio.set_event_loop(asyncio.new_event_loop())
     server = websockets.serve(hello, 'localhost', bot_id + 1000)
     print('API server started, listening on ', bot_id + 1000)
     asyncio.get_event_loop().run_until_complete(server)
     asyncio.get_event_loop().run_forever()
+
+
+if __name__ == '__main__':
+    start_server(0)
