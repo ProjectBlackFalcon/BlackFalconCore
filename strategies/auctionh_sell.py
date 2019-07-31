32785c94322e85e4c52af87bc1518fba78e6d5df
avialaret <alexisvlrt@gmail.com>
2019-07-31 19:29:34 +0200

    Added auction house sell function

--- strategies/auctionh_sell.py
+++ strategies/auctionh_sell.py
@@ -3,7 +3,7 @@ import math
 import time
 
 import strategies
-from strategies import support_functions, auctionh_get_prices
+from strategies import support_functions, auctionh_get_prices, auctionh_close, auctionh_open
 from tools import logger as log
 
 
@@ -42,6 +42,7 @@ def auctionh_sell(**kwargs):
                 if inventory_item['objectGID'] == item['general_id'] and inventory_item['position'] == 63:
                     item['name'] = assets['id_2_names'][str(inventory_item['objectGID'])]
                     item['type'] = assets['id_2_type'][str(inventory_item['objectGID'])]
+                    item['level'] = assets['id_2_level'][str(inventory_item['objectGID'])]
                     stuff_to_sell[inventory_item['objectUID']] = item
 
         # If a unique is is given
@@ -61,6 +62,7 @@ def auctionh_sell(**kwargs):
                     found = True
                     item['name'] = assets['id_2_names'][str(inventory_item['objectGID'])]
                     item['type'] = assets['id_2_type'][str(inventory_item['objectGID'])]
+                    item['level'] = assets['id_2_level'][str(inventory_item['objectGID'])]
                     item['general_id'] = inventory_item['objectGID']
                     stuff_to_sell[inventory_item['objectUID']] = item
 
@@ -76,7 +78,7 @@ def auctionh_sell(**kwargs):
 
     # Check quantities
     for uid, item in stuff_to_sell.items():
-        if 'quantity' not in item.keys():
+        if 'quantity' not in item.keys() or item['quantity'] is None:
             item['quantity'] = 'all'
 
         available_in_inv = 0
@@ -97,40 +99,41 @@ def auctionh_sell(**kwargs):
             return strategy
 
     # Fill prices
+    prices_to_check = []
     for uid, item in stuff_to_sell.items():
-        prices_to_check = []
-        if 'price' not in item.keys():
+        if 'price' not in item.keys() or item['quantity'] is None:
             prices_to_check.append(item['general_id'])
 
-    sub_strategy = strategies.auctionh_get_prices.auctionh_get_prices(
-        assets=assets,
-        orders_queue=orders_queue,
-        listener=listener,
-        strategy={
-            'bot': strategy['bot'],
-            'parameters': {
-                'general_ids_list': prices_to_check
+    if len(prices_to_check):
+        sub_strategy = strategies.auctionh_get_prices.auctionh_get_prices(
+            assets=assets,
+            orders_queue=orders_queue,
+            listener=listener,
+            strategy={
+                'bot': strategy['bot'],
+                'parameters': {
+                    'general_ids_list': prices_to_check
+                }
             }
-        }
-    )
-    if not sub_strategy['report']['success']:
-        strategy['report'] = {
-            'success': False,
-            'details': {'Execution time': time.time() - start, 'Reason': sub_strategy['report']}
-        }
-        log.close_logger(logger)
-        return strategy
+        )
+        if not sub_strategy['report']['success']:
+            strategy['report'] = {
+                'success': False,
+                'details': {'Execution time': time.time() - start, 'Reason': sub_strategy['report']}
+            }
+            log.close_logger(logger)
+            return strategy
 
-    for gid, item in sub_strategy['report']['details']['Results'].items():
-        gid = int(gid)
-        for uid, item_to_sell in stuff_to_sell.items():
-            if gid == item_to_sell['id'] and 'price' not in item_to_sell.keys():
-                item_to_sell['price'] = item['item_stats'][0]['prices'][[1, 10, 100].index(item_to_sell['pack_size'])] - 1
+        for gid, item in sub_strategy['report']['details']['Results'].items():
+            gid = int(gid)
+            for uid, item_to_sell in stuff_to_sell.items():
+                if gid == item_to_sell['general_id'] and 'price' not in item_to_sell.keys():
+                    item_to_sell['price'] = item['items_stats'][0]['prices'][[1, 10, 100].index(item_to_sell['pack_size'])] - 1
 
     # Check that the bot has enough money to sell
     total_fee = 0
     for uid, item in stuff_to_sell.items():
-        total_fee += math.ceil(item['price'] * item['quantity'] * 0.2)
+        total_fee += math.ceil(item['price'] * item['quantity'] * 0.02)
     if total_fee > listener.game_state['kamas']:
         logger.warning('Not enough money to sell items. Needed: {}, available: {}. Items to sell: {}'.format(total_fee, listener.game_state['kamas'], stuff_to_sell))
         strategy['report'] = {
@@ -142,7 +145,7 @@ def auctionh_sell(**kwargs):
         return strategy
 
     # Open the auction house if it is closed.
-    if not listener.game_state['auction_house_info']:
+    if not listener.game_state['auction_house_info'] or 'sellerDescriptor' not in listener.game_state['auction_house_info'].keys():
         sub_strategy = strategies.auctionh_open.auctionh_open(
             listener=listener,
             orders_queue=orders_queue,
@@ -162,9 +165,20 @@ def auctionh_sell(**kwargs):
             log.close_logger(logger)
             return strategy
 
-    # TODO: Check that the hdv has the level to sell these items
+    # Check that the hdv has the level to sell these items
+    for uid, item in stuff_to_sell.items():
+        if item['level'] > listener.game_state['auction_house_info']['sellerDescriptor']['maxItemLevel']:
+            logger.warning('Item {}/() is too high level for the {} auction house. Item level: {}, auction house max: {}'.format(item['general_id'], item['name'], listener.game_state['pos'], item['level'], listener.game_state['auction_house_info']['sellerDescriptor']['maxItemLevel']))
+            strategy['report'] = {
+                'success': False,
+                'details': {'Execution time': time.time() - start,
+                            'Reason': 'Item {}/() is too high level for the {} auction house. Item level: {}, auction house max: {}'.format(item['general_id'], item['name'], listener.game_state['pos'], item['level'], listener.game_state['auction_house_info']['sellerDescriptor']['maxItemLevel'])}
+            }
+            log.close_logger(logger)
+            return strategy
 
     # Check that the hdv can sell these items
+    # TODO: test
     for uid, item in stuff_to_sell.items():
         if item['type'] not in listener.game_state['auction_house_info']['sellerDescriptor']['types']:
             logger.warning('Type {} can not be sold at the {} auction house'.format(item['type'], listener.game_state['pos']))
@@ -177,11 +191,12 @@ def auctionh_sell(**kwargs):
             return strategy
 
     # Check that the bot has enough slots available
+    # TODO: test
     total_slots_needed = 0
     for uid, item in stuff_to_sell.items():
         total_slots_needed += item['quantity']
 
-    available_slots = listener.game_state['auction_house_info']['sellerDescriptor']['maxItemPerAccount'] - len(listener.game_state['auction_house_info']['sellerDescriptor']['objectsInfos'])  # TODO: verify that
+    available_slots = listener.game_state['auction_house_info']['sellerDescriptor']['maxItemPerAccount'] - len(listener.game_state['auction_house_info']['objectsInfos'])
     if total_slots_needed > available_slots:
         logger.warning('Not enough slots available to sell items. Needed: {}, available: {}. Items to sell: {}'.format(total_slots_needed, available_slots, stuff_to_sell))
         strategy['report'] = {
@@ -192,5 +207,80 @@ def auctionh_sell(**kwargs):
         log.close_logger(logger)
         return strategy
 
+    # Sell stuff
+    stuff_sold = {}
+    for uid, item in stuff_to_sell.items():
+        for _ in range(item['quantity']):
+            previous_item_quantity = None
+            for inv_item in listener.game_state['inventory']:
+                if inv_item['objectGID'] == item['general_id']:
+                    previous_item_quantity = inv_item['quantity']
+            order = {
+                'command': 'auctionh_sell_item',
+                'parameters': {
+                    'unique_id': uid,
+                    'quantity': item['pack_size'],
+                    'price': item['price']
+                }
+            }
+            logger.info('Sending order to bot API: {}'.format(order))
+            orders_queue.put((json.dumps(order),))
 
-    # TODO: sell stuff
+            start = time.time()
+            timeout = 10 if 'timeout' not in strategy.keys() else strategy['timeout']
+            waiting = True
+            while waiting and time.time() - start < timeout:
+                item_quantity = None
+                for inv_item in listener.game_state['inventory']:
+                    if inv_item['objectGID'] == item['general_id']:
+                        item_quantity = inv_item['quantity']
+                if item_quantity != previous_item_quantity:
+                    waiting = False
+                time.sleep(0.05)
+            execution_time = time.time() - start
+
+            if waiting:
+                logger.warning('Failed to sell {}/{}, quantity: {}, price: {}'.format(item['general_id'], item['name'], item['pack_size'], item['price']))
+                strategy['report'] = {
+                    'success': False,
+                    'details': {'Execution time': execution_time,
+                                'Reason': 'Failed to sell {}:/}, quantity: {}, price: {}'.format(item['general_id'], item['name'], item['pack_size'], item['price'])}
+                }
+                log.close_logger(logger)
+                return strategy
+            logger.info('Sold {}/{}, quantity: {}, price: {}'.format(item['general_id'], item['name'], item['pack_size'], item['price']))
+            if uid in stuff_sold.keys():
+                stuff_sold[uid]['quantity'] += item['pack_size']
+                stuff_sold[uid]['total_price'] += item['price']
+            else:
+                stuff_sold[uid] = {
+                    'name': item['name'],
+                    'general_id': item['general_id'],
+                    'quantity': item['pack_size'],
+                    'total_price': item['price']
+                }
+
+    # Close auction house
+    sub_strategy = strategies.auctionh_close.auctionh_close(
+        listener=listener,
+        orders_queue=orders_queue,
+        assets=assets,
+        strategy={
+            "bot": strategy['bot'],
+        }
+    )
+    if not sub_strategy['report']['success']:
+        strategy['report'] = {
+            'success': False,
+            'details': {'Execution time': time.time() - start, 'Reason': sub_strategy['report']}
+        }
+        log.close_logger(logger)
+        return strategy
+
+    strategy['report'] = {
+        'success': True,
+        'details': {'Execution time': time.time() - global_start, 'sold': stuff_sold}
+    }
+
+    log.close_logger(logger)
+    return strategy
