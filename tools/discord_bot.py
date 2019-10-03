61f2f9f476078b63edc6f2f7fa0a42c52b157143
Ugdha <alexisvlrt@gmail.com>
2019-10-03 22:10:09 +0200

    Shit fix for multi lines messages

--- tools/discord_bot.py
+++ tools/discord_bot.py
@@ -1,12 +1,20 @@
 import os
+
+import time
+
 from credentials import credentials
 from subprocess import Popen, PIPE
 
 
 def send_discord_message(message):
     sh_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'discord.sh'))
-    Popen('''bash {} --webhook-url={} --text "{}"'''.format(sh_path, credentials['discord']['webhook'], message), shell=True, stdout=PIPE)
+    Popen('''bash {} --webhook-url={} --text "{}"'''.format(sh_path, credentials['discord']['webhook'], message.replace('`', "\`").replace('\n', ' ' * 300)), shell=True, stdout=PIPE)
 
 
 if __name__ == '__main__':
-    send_discord_message('Python integration test')
+    send_discord_message('''Traceback (most recent call last):
+  File "swarm_node/swarm_node.py", line 111, in maintain_assets
+    raise Exception
+Exception
+'''.replace('\n', ' '*500))
+    time.sleep(10)
